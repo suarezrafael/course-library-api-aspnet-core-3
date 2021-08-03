4cbe2bf477d37e33bf924c6c26e33149af004397
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-03 14:35:34 -0300

    Criando o arquivo profile para o AutoMapper

---
+++ CourseLibrary.API/Profiles/AuthorsProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+                
+        }
+
+    }
+}
