4ad125e128c33bdbf6ee9582a190379f23aacd3f
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-03 14:27:25 -0300

    Separando entity model do dto

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -1,4 +1,6 @@
-﻿using CourseLibrary.API.Services;
+﻿using CourseLibrary.API.Helpers;
+using CourseLibrary.API.Models;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,28 +11,47 @@ namespace CourseLibrary.API.Controllers
 {
     [ApiController]
     [Route("api/authors")]
-    public class AuthorsController
+    public class AuthorsController : ControllerBase
     {
         private readonly ICourseLibraryRepository _courseLibraryRepository;
 
         public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
         {
-            _courseLibraryRepository = courseLibraryRepository ?? 
+            _courseLibraryRepository = courseLibraryRepository ??
                 throw new ArgumentNullException(nameof(courseLibraryRepository));
         }
 
         [HttpGet()]
-        public IActionResult GetAuthors()
+        public ActionResult<AuthorDto> GetAuthors()
         {
-            var authorsFrmoRepo = _courseLibraryRepository.GetAuthors();
-            return new JsonResult(authorsFrmoRepo);
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            var authors = new List<AuthorDto>();
+
+            foreach (var author in authorsFromRepo)
+            {
+                authors.Add(
+                    new AuthorDto() { 
+                        Id = author.Id,
+                        Name = $"{author.FirstName} {author.LastName}",
+                        MainCategory = author.MainCategory,
+                        Age = author.DateOfBirth.GetCurrentAge()
+                    }
+                    );
+            }
+            return Ok(authorsFromRepo);
         }
 
         [HttpGet("{authorId}")]
         public IActionResult GetAuthor(Guid authorId)
         {
-            var author = _courseLibraryRepository.GetAuthor(authorId);
-            return new JsonResult(author);
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(authorFromRepo);
         }
     }
 }
--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -22,15 +22,19 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers();
-             
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+
+            }).AddXmlDataContractSerializerFormatters();
+
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
             services.AddDbContext<CourseLibraryContext>(options =>
             {
                 options.UseSqlServer(
                     @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
-            }); 
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
