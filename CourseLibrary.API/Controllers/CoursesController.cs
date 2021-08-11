8d2baea5669f919293883b46bfb01f80ac2dd60b
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-11 16:42:04 -0300

    ApplyTo com suporte a invalid propertie

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -152,7 +152,7 @@ namespace CourseLibrary.API.Controllers
 
             var courseToPatch = _mapper.Map<CourseForUpdateDto>(courseForAuthorFromRepo);
             // add validation
-            patchDocument.ApplyTo(courseToPatch);
+            patchDocument.ApplyTo(courseToPatch,ModelState);
 
             if (!TryValidateModel(courseToPatch))
             {
