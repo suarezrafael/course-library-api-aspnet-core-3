14b2d0237ed88c4e405e78a99a9331e28f98df74
Rafael Suarez <rafaelv_s@hotmail.com>
2021-08-11 16:48:44 -0300

    detalhes da resposta de validatonproblems

--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -3,6 +3,10 @@ using CourseLibrary.API.Models;
 using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -117,8 +121,8 @@ namespace CourseLibrary.API.Controllers
 
         [HttpPatch("{courseId}")]
         public ActionResult PartiallyUpdateCourseForAuthor(Guid authorId,
-    Guid courseId,
-    JsonPatchDocument<CourseForUpdateDto> patchDocument)
+            Guid courseId,
+            JsonPatchDocument<CourseForUpdateDto> patchDocument)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
             {
@@ -166,6 +170,14 @@ namespace CourseLibrary.API.Controllers
             _courseLibraryRepository.Save();
 
             return NoContent();
+        }
+
+        public override ActionResult ValidationProblem(
+        [ActionResultObjectValue] ModelStateDictionary modelStateDictionary)
+        {
+            var options = HttpContext.RequestServices
+                .GetRequiredService<IOptions<ApiBehaviorOptions>>();
+            return (ActionResult)options.Value.InvalidModelStateResponseFactory(ControllerContext);
         }
     }
 }
